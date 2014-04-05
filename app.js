fc68d918fe05e42836e830b89f1157d43e40511b
Alessandro Segala <alessandro.segala@gmail.com>
2014-04-05 12:08:59 -0400

    Fixing login

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
 var restify = require('restify')
+var session = require('./lib/session')
 
 // Load environments
 var config = require(__dirname + '/lib/environment')()
@@ -16,6 +17,7 @@ var server = restify.createServer({
 
 // Save the config object in the server
 server.appConfig = config
+server.session = session
 
 server.use(restify.acceptParser(server.acceptable))
 server.use(restify.queryParser())
@@ -27,8 +29,21 @@ server.use(restify.CORS({
 	origins: config.allowOrigin ? config.allowOrigin : '*'
 }))
 
+server.pre(function(req, res, next) {
+	res.refreshSession = false
+	res.session = req.header('X-Session-Token', '')
+	return next()
+})
+
 // Routing
 require('./routes')(server, restify)
+
+server.use(function(req, res, next) {
+	if(res.refreshSession) {
+		console.log('Refreshing session')
+		res.header('X-Session-Token', res.session)
+	}
+})
 
 server.listen(config.httpPort, function () {
 	console.log('%s listening at %s', server.name, server.url)
--- routes/login.js
+++ routes/login.js
@@ -1,31 +1,34 @@
 var mongo = require('mongodb')
 var NodePbkdf2 = require('node-pbkdf2')
-var session = require('../lib/session')
 
 var users =
 	[{"_id":"534021435c35eb8c10339233","email":"alessandro.segala@gmail.com","password":"7eZCp9M8SuXF::LG6erTrV0c3Fhvlr+QtE59TIcILTTbXam0xA2nQN::30::10000"}]
 
 module.exports = function(server, restify){
 	server.post('/login', function (req, res, next) {
-		var email = req.email
-		var password = req.password
+		var session = server.session
+		
+		var email = req.params.email
+		var password = req.params.password
 		
 		var hasher = new NodePbkdf2({ iterations: 10000, saltLength: 12, derivedKeyLength: 30 })
 		
 		var i = users.length
 		var encryptedPassword = false
+		var user = false
 		while(i--) {
 			if(users[i].email == email) {
-				encryptedPassword = users[i].password
+				user = users[i]
+				break
 			}
 		}
 		
-		if(!encryptedPassword) {
+		if(!user) {
 			return next(new restify.InvalidCredentialsError('Email or password wrong'))
 		}
 		
 		var passOk = false
-		if(hasher.checkPassword(password, encryptedPassword, function(err, status) {
+		if(hasher.checkPassword(password, user.password, function(err, status) {
 			passOk = status
 		}))
 		
@@ -33,8 +36,9 @@ module.exports = function(server, restify){
 			return next(new restify.InvalidCredentialsError('Email or password wrong'))
 		}
 		
-		console.log(session.create)
+		res.session = session.create(user._id)
 		
+		res.refreshSession = true
 		res.send({result: 1})
 		return next()
 	})
