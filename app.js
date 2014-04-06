f45b556ba3198acfe7bd12fca55303da673c3861
Bilal Akhtar <me@itsbilal.com>
2014-04-06 12:27:00 -0400

    Implement options calls

--- app.js
+++ app.js
@@ -25,14 +25,33 @@ server.use(restify.bodyParser())
 server.use(restify.gzipResponse())
 
 // Enable Cross-Origin Resource Sharing (CORS)
-server.use(restify.CORS({
-	origins: config.allowOrigin ? config.allowOrigin : '*'
-}))
+server.use(restify.CORS({"headers":["X-Session-Token"]}))
+server.use(restify.fullResponse())
 
+server.on('MethodNotAllowed', function(req, res) {
+if (req.method.toLowerCase() == "options") {
+	res.header('Access-Control-Allow-Headers', 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, Api-Version, Response-Time, X-Session-Token');
+	res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Headers', 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, Api-Version, Response-Time, X-Session-Token');
+        res.header('Access-Control-Allow-Methods', ['OPTIONS','GET', 'POST'].join(', '));
+        res.header('Access-Control-Allow-Origin', '*');
+
+	return res.send(204)
+}else {
+	return res.send(405)
+}
+})
+
+server.pre(restify.pre.userAgentConnection());
 server.pre(function(req, res, next) {
 	var token = req.header('X-Session-Token', '')
 
 	res.header('Application-Type', 'application/json')
+	res.header('Access-Control-Allow-Credentials', true);
+    	res.header('Access-Control-Allow-Headers', 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, Api-Version, Response-Time, X-Session-Token');
+    	res.header('Access-Control-Allow-Methods', ['OPTIONS','GET', 'POST'].join(', '));
+    	res.header('Access-Control-Allow-Origin', '*');
+	
 	restify.pre.sanitizePath()
 	
 	session.init(token)
